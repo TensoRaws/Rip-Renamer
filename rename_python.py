f99a6c10612d94ceedd8ac5e79de4a7cb07d656c
hyw-dev <68835291+hyw-dev@users.noreply.github.com>
2023-06-08 02:19:32 +0800

    Update rename_python.py

--- rename_python.py
+++ rename_python.py
@@ -45,7 +45,7 @@ def rename(fi):
     # - NN -> [NN]
     pos = fi.find('- ')
     if pos != -1 and fi[pos + 2: pos + 4].isnumeric():
-        fi = fi.replace(fi[pos: pos + 4], fi[pos + 2: pos + 4])
+        fi = fi.replace(fi[pos: pos + 4], f'[{fi[pos + 2: pos + 4]}]')
 
     e_list = ['NCOP', 'NCED', 'OVA', 'SP']
     pos = -1
